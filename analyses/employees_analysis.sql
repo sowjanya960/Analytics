5009633129ef057a82a45cf374cebc0a61bfc39f
Sowjanya Chandra <sowjanyachandra18@gmail.com>
2026-07-29 09:48:40 +0000

    analyses sample

--- analyses/employees_analysis.sql
+++ analyses/employees_analysis.sql
@@ -4,10 +4,13 @@
 
 select src.source_code,src_counts,tgt_counts,src_counts-tgt_counts as diff_cnt ,src_salary,trgt_salary,
 src_salary-trgt_salary as diff_salary
-(select 'Employee' as source_code,count(*) as src_counts ,salary as src_salary  FROM {{source('ANALYTICS','EMPLOYEES')}} ) src
+(select 'Employee' as source_code,count(*) as src_counts ,salary as src_salary 
+ FROM {{source('ANALYTICS','EMPLOYEES')}} group by all) src
 inner join
 ( select * from
 (select  source_code,sum(counts) as tgt_counts,trgt_salary from  
-( SELECT 'Employee' AS source_code, count(*) as counts,employee_salary as trgt_salary  FROM {{ref('employees_table')}}
-)group by source_code )) tgt
+( SELECT 'Employee' AS source_code, count(*) as counts,employee_salary as trgt_salary 
+ FROM {{ref('employees_table')}}
+ group by all
+)group by all )) tgt
 on tgt.source_code =src.source_code
