3c8252384e8f298330a37d36ea14cf5c5a12cf60
Sowjanya Chandra <sowjanyachandra18@gmail.com>
2026-07-29 16:05:38 +0000

    snapshot practice

--- snapshots/Employees_Snapshot/employees_snapshot.sql
+++ snapshots/Employees_Snapshot/employees_snapshot.sql
@@ -1,16 +1,16 @@
-{ %snapshot EMPLOYEES_SNAPSHOT%}
+{% snapshot EMPLOYEES_SNAPSHOT %}
 
 {{
 
     config 
     (
       unique_key ='emp_id',
-       strategy = timestamp,
-       updated_at = updated_at
+       strategy = 'timestamp',
+       updated_at = 'updated_at'
     )
 
 }}
 
 select * from {{source('ANALYTICS','EMPLOYEES')}}
 
- { % endsnapshot % }
+ {% endsnapshot %}
